2168c13cbcc212bbb7f2538e59133589db178558
임선민 <imsnmn@imseonmin-ui-MacBookAir.local>
2024-04-11 15:27:43 +0900

    Initial commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function FirstPost() {
+  return (
+    <>
+    <h1><div>My name is Sunmin</div> 
+<img src="https://i.pinimg.com/736x/5d/6d/23/5d6d23fd7adb44baba20a60c252da339.jpg"></img>
+      </h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </>
+  );
+}
